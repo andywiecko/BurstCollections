ab08702d98e1f09850ad2dcf5646f4d7b42e3753
Andrzej Więckowski <andywiecko@gmail.com>
2022-04-22 17:36:25 +0200

    feat: bounding volume tree vs tree intersections
    NativeBoundingVolumeTree<AABB> tree vs. tree intersections implementation using queue.
    The result is stored in NativeList<int2>.

--- Runtime/NativeBoundingVolumeTree/NativeBoundingVolumeTreeExtensions.cs
+++ Runtime/NativeBoundingVolumeTree/NativeBoundingVolumeTreeExtensions.cs
@@ -10,7 +10,6 @@ namespace andywiecko.BurstCollections
         public static void GetIntersectionsWithAABB(this ref BoundingVolumeTree<AABB> tree, AABB aabb, NativeList<int> result) => tree.tree.GetIntersectionsWithAABB(aabb, result);
 
         #region Tree vs. AABB intersections
-
         /// <summary>
         /// Collects all intersections of the <paramref name="tree"/> with given <paramref name="aabb"/>
         /// and stores them in the <paramref name="result"/>.
@@ -19,11 +18,8 @@ namespace andywiecko.BurstCollections
         /// <remarks>
         /// It allocates temporary <see cref="NativeQueue{T}"/> internally.
         /// </remarks>
-        public static void GetIntersectionsWithAABB(this NativeBoundingVolumeTree<AABB> tree, AABB aabb, NativeList<int> result)
-        {
-            using var queue = new NativeQueue<int>(Allocator.Temp);
-            GetIntersectionsWithAABB(tree, aabb, result, queue);
-        }
+        public static void GetIntersectionsWithAABB(this NativeBoundingVolumeTree<AABB> tree, AABB aabb, NativeList<int> result) =>
+            GetIntersectionsWithAABB(tree.AsReadOnly(), aabb, result);
 
         /// <summary>
         /// Collects all intersections of the <paramref name="tree"/> with given <paramref name="aabb"/>
@@ -64,6 +60,93 @@ namespace andywiecko.BurstCollections
                         result.Add(id);
                     }
                     bfs.Traverse(id);
+                }
+            }
+        }
+        #endregion
+
+        #region Tree vs. Tree intersections
+        /// <summary>
+        /// Collects all intersections of the <paramref name="tree"/> with <paramref name="otherTree"/>
+        /// and stores them in the <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">Indicies of the <paramref name="tree"/> and <paramref name="otherTree"/> leaves which intersects.</param>
+        /// <remarks>
+        /// It allocates temporary <see cref="NativeQueue{T}"/> internally.
+        /// </remarks>
+        public static void GetIntersectionsWithTree(this NativeBoundingVolumeTree<AABB> tree, NativeBoundingVolumeTree<AABB> otherTree, NativeList<int2> result) =>
+            GetIntersectionsWithTree(tree.AsReadOnly(), otherTree.AsReadOnly(), result);
+
+        /// <summary>
+        /// Collects all intersections of the <paramref name="tree"/> with <paramref name="otherTree"/>
+        /// and stores them in the <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">Indicies of the <paramref name="tree"/> and <paramref name="otherTree"/> leaves which intersects.</param>
+        /// <remarks>
+        /// It allocates temporary <see cref="NativeQueue{T}"/> internally.
+        /// </remarks>
+        public static void GetIntersectionsWithTree(this NativeBoundingVolumeTree<AABB>.ReadOnly tree, NativeBoundingVolumeTree<AABB>.ReadOnly otherTree, NativeList<int2> result)
+        {
+            using var queue = new NativeQueue<int2>(Allocator.Temp);
+            GetIntersectionsWithTree(tree, otherTree, result, queue);
+        }
+
+        /// <summary>
+        /// Collects all intersections of the <paramref name="tree"/> with <paramref name="otherTree"/>
+        /// and stores them in the <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">Indicies of the <paramref name="tree"/> and <paramref name="otherTree"/> leaves which intersects.</param>
+        public static void GetIntersectionsWithTree(this NativeBoundingVolumeTree<AABB> tree, NativeBoundingVolumeTree<AABB> otherTree, NativeList<int2> result, NativeQueue<int2> queue) => GetIntersectionsWithTree(tree.AsReadOnly(), otherTree.AsReadOnly(), result, queue);
+
+        /// <summary>
+        /// Collects all intersections of the <paramref name="tree"/> with <paramref name="otherTree"/>
+        /// and stores them in the <paramref name="result"/>.
+        /// </summary>
+        /// <param name="result">Indicies of the <paramref name="tree"/> and <paramref name="otherTree"/> leaves which intersects.</param>
+        public static void GetIntersectionsWithTree(this NativeBoundingVolumeTree<AABB>.ReadOnly tree, NativeBoundingVolumeTree<AABB>.ReadOnly otherTree, NativeList<int2> result, NativeQueue<int2> queue)
+        {
+            if (tree.IsEmpty || otherTree.IsEmpty)
+            {
+                return;
+            }
+
+            queue.Clear();
+            queue.Enqueue(new(tree.RootId.Value, otherTree.RootId.Value));
+
+            while (queue.TryDequeue(out int2 ids))
+            {
+                var idA = ids.x;
+                var idB = ids.y;
+                var a = tree.Volumes[idA];
+                var b = otherTree.Volumes[idB];
+
+                if (a.Intersects(b))
+                {
+                    var na = tree.Nodes[idA];
+                    var nb = otherTree.Nodes[idB];
+                    switch (na.IsLeaf, nb.IsLeaf)
+                    {
+                        case (true, true):
+                            result.Add(ids);
+                            continue;
+
+                        case (false, false):
+                            queue.Enqueue(new(na.LeftChildId, nb.LeftChildId));
+                            queue.Enqueue(new(na.LeftChildId, nb.RightChildId));
+                            queue.Enqueue(new(na.RightChildId, nb.LeftChildId));
+                            queue.Enqueue(new(na.RightChildId, nb.RightChildId));
+                            continue;
+
+                        case (true, false):
+                            queue.Enqueue(new(idA, nb.LeftChildId));
+                            queue.Enqueue(new(idA, nb.RightChildId));
+                            continue;
+
+                        case (false, true):
+                            queue.Enqueue(new(na.LeftChildId, idB));
+                            queue.Enqueue(new(na.RightChildId, idB));
+                            continue;
+                    }
                 }
             }
         }
--- Tests/NativeBoundingVolumeTreeExtensionsEditorTests.cs
+++ Tests/NativeBoundingVolumeTreeExtensionsEditorTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Linq;
 using Unity.Collections;
 using Unity.Mathematics;
 
@@ -66,6 +67,77 @@ namespace andywiecko.BurstCollections.Editor.Tests
 
             tree.Dispose();
             result.Dispose();
+        }
+
+        private static readonly AABB UnitAABB = new AABB(0, 1);
+
+        private static readonly TestCaseData[] intersectionsWithOtherTreeTestData = new[]
+        {
+            new TestCaseData(
+                new[]{ UnitAABB, UnitAABB, UnitAABB, UnitAABB },
+                new[]{ UnitAABB.Translate(3), UnitAABB.Translate(3) }
+            ){ TestName = "Test case 1 (empty result)", ExpectedResult = new int2[]{ } },
+
+            new TestCaseData(
+                new[]
+                {
+                    new AABB(math.float2(0, 0), math.float2(1, 1)),
+                    new AABB(math.float2(1, 0), math.float2(2, 1)),
+                    new AABB(math.float2(1, 1), math.float2(2, 2)),
+                    new AABB(math.float2(0, 1), math.float2(1, 2)),
+                },
+                new[]
+                {
+                    new AABB(math.float2(0, 0), math.float2(1, 1)).Translate(1.5f),
+                    new AABB(math.float2(1, 0), math.float2(2, 1)).Translate(1.5f),
+                    new AABB(math.float2(1, 1), math.float2(2, 2)).Translate(1.5f),
+                    new AABB(math.float2(0, 1), math.float2(1, 2)).Translate(1.5f),
+                }
+            ){ TestName = "Test case 2 (single intersection)", ExpectedResult = new int2[]{ math.int2(2, 0) } },
+
+            new TestCaseData(
+                new[]
+                {
+                    UnitAABB.Translate(0),
+                    UnitAABB.Translate(2),
+                    UnitAABB.Translate(4),
+                    UnitAABB.Translate(6),
+                },
+                new[]
+                {
+                    UnitAABB.Translate(0),
+                    UnitAABB.Translate(2),
+                    UnitAABB.Translate(4),
+                    UnitAABB.Translate(6),
+                }
+            ){ TestName = "Test case 3 (\"self\" intersection)", ExpectedResult = new[]{ math.int2(0, 0), math.int2(1, 1), math.int2(2, 2), math.int2(3, 3) } },
+
+            new TestCaseData(
+                new[]{ UnitAABB, UnitAABB, UnitAABB },
+                new[]{ UnitAABB, UnitAABB, UnitAABB }
+            ){ TestName = "Test case 4 (all with all)", ExpectedResult = new[]
+            {
+                math.int2(0, 0), math.int2(0, 1), math.int2(0, 2),
+                math.int2(1, 0), math.int2(1, 1), math.int2(1, 2),
+                math.int2(2, 0), math.int2(2, 1), math.int2(2, 2),
+            } },
+        };
+
+        [Test, TestCaseSource(nameof(intersectionsWithOtherTreeTestData))]
+        public int2[] IntersectionsWithOtherTreeTest(AABB[] managedVolumes1, AABB[] managedVolumes2)
+        {
+            using var volumes1 = new NativeArray<AABB>(managedVolumes1, Allocator.Persistent);
+            using var volumes2 = new NativeArray<AABB>(managedVolumes2, Allocator.Persistent);
+            using var tree1 = new NativeBoundingVolumeTree<AABB>(managedVolumes1.Length, Allocator.Persistent);
+            using var tree2 = new NativeBoundingVolumeTree<AABB>(managedVolumes2.Length, Allocator.Persistent);
+            using var result = new NativeList<int2>(64, Allocator.Persistent);
+
+            tree1.Construct(volumes1.AsReadOnly());
+            tree2.Construct(volumes2.AsReadOnly());
+
+            tree1.GetIntersectionsWithTree(tree2, result);
+
+            return result.ToArray().OrderBy(i => i.x).ThenBy(i => i.y).ToArray();
         }
     }
 }
