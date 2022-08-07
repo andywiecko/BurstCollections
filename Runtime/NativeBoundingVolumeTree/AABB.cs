dad87ef4231050d806ef486c1446f820e365486c
Andrzej Więckowski <andywiecko@gmail.com>
2022-08-07 16:14:08 +0200

    feat: aabb additional properties

--- Runtime/NativeBoundingVolumeTree/AABB.cs
+++ Runtime/NativeBoundingVolumeTree/AABB.cs
@@ -10,20 +10,17 @@ namespace andywiecko.BurstCollections
     /// </remarks>
     public readonly struct AABB : IBoundingVolume<AABB>
     {
-        public readonly float Volume { get; }
-        public readonly float2 Min;
-        public readonly float2 Max;
+        public readonly float Volume => (Max - Min).x * (Max - Min).y;
+        public readonly float2 Center => Min + 0.5f * Size;
+        public readonly float2 Size => Max - Min;
 
-        public AABB(float2 min, float2 max)
-        {
-            Min = min;
-            Max = max;
-            Volume = (Max - Min).x * (Max - Min).y;
-        }
+        public readonly float2 Min, Max;
+        public AABB(float2 min, float2 max) => (Min, Max) = (min, max);
         public bool Contains(float2 point) => math.all(point >= Min & point <= Max);
         public bool Intersects(AABB other) => math.all(Max >= other.Min) && math.all(Min <= other.Max);
         public AABB Union(AABB other) => new AABB(math.min(Min, other.Min), math.max(Max, other.Max));
         public void Deconstruct(out float2 min, out float2 max) => (min, max) = (Min, Max);
         public AABB Translate(float2 t) => new AABB(Min + t, Max + t);
+        public override string ToString() => $"(min: {Min}, max: {Max})";
     }
 }
